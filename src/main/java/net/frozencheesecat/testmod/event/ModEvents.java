e3eede4fb857e3810f539fea181b4989fe18414c
IceFong <ice2002.5@gmail.com>
2023-08-01 19:49:54 +0800

    NULL

--- src/main/java/net/frozencheesecat/testmod/event/ModEvents.java
+++ src/main/java/net/frozencheesecat/testmod/event/ModEvents.java
@@ -3,7 +3,11 @@ package net.frozencheesecat.testmod.event;
 import net.frozencheesecat.testmod.TestMod;
 import net.frozencheesecat.testmod.entity.ModEntities;
 import net.frozencheesecat.testmod.entity.custom.TigerEntity;
+import net.minecraft.world.entity.SpawnPlacements;
+import net.minecraft.world.entity.animal.Animal;
+import net.minecraft.world.level.levelgen.Heightmap;
 import net.minecraftforge.event.entity.EntityAttributeCreationEvent;
+import net.minecraftforge.event.entity.SpawnPlacementRegisterEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 
@@ -13,6 +17,12 @@ public class ModEvents {
     @SubscribeEvent
     public static void entityAttributeEvent(EntityAttributeCreationEvent event) {
         event.put(ModEntities.TIGER.get(), TigerEntity.setAttributes());
+    }
+
+    @SubscribeEvent
+    public static void entitySpawnRestriction(SpawnPlacementRegisterEvent event) {
+        event.register(ModEntities.TIGER.get(), SpawnPlacements.Type.ON_GROUND, Heightmap.Types.MOTION_BLOCKING_NO_LEAVES,
+            Animal::checkAnimalSpawnRules, SpawnPlacementRegisterEvent.Operation.REPLACE);
     }
 
 }
